3223d788a6217c408e305fd6e0d536ca82d654ca
Mateusz Kryszczak <Mateusz.Kryszczak@fenergo.com>
2020-11-12 19:40:02 +0100

    version 2 init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Appv1 from "./v1/App";
+import reportWebVitals from "./reportWebVitals";
+
+const Root = () => {
+  const [version, setVersion] = useState(null);
+
+  if (version === null) {
+    return (
+      <>
+        <button onClick={() => setVersion(1)}>Version 1</button>
+        <button onClick={() => setVersion(2)}>Version 2</button>
+      </>
+    );
+  } else if (version === 1) {
+    return <Appv1 />;
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
